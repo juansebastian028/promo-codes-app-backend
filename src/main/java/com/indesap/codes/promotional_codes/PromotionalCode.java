39fc0dda9f5b370d56688f57a2a40c79dae79771
Juan <juansebastianvargasposada@gmail.com>
2024-08-31 22:36:27 -0500

    some changes

--- src/main/java/com/indesap/codes/promotional_codes/PromotionalCode.java
+++ src/main/java/com/indesap/codes/promotional_codes/PromotionalCode.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "PROMOTIONAL_CODE")
+@Table(name = "PROMOTIONAL_CODE", uniqueConstraints = { @UniqueConstraint(columnNames = {"code"}) })
 @Data
 @Builder
 @AllArgsConstructor
